9c0f3f5c4bb32d90b8cb46d73a94284bb3914041
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2017-09-06 11:57:14 -0300

    ejercicio 1

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include "../Pila/Pila.h"
+using namespace std;
 
 int main() {
     std::cout << "Ejercicio 03/01\n" << std::endl;
+
+    Pila<char> p;
+    char palabra[100];
+    int i, c=0;
+
+    cin >> palabra;
+
+    for(i=0; palabra[i]!='\0'; i++)
+        c++;
+
+    i=0;
+    while(i<c){
+        p.push(palabra[i]);
+        i++;
+    }
+
+    i=0;
+    while(i<c){
+        palabra[i]=p.pop();
+        i++;
+    }
+
+    cout << palabra;
+
     return 0;
 }
